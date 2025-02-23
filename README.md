10828590d59c2d0b782a95ddd890f12afd5ee531
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2025-02-23 16:55:57 -0500

    Initial commit, hides read categories.

---
+++ README.md
@@ -0,0 +1,5 @@
+## Hide Read Categories in GMail
+
+![test](https://github.com/dblock/hide-read-categories/workflows/build%20CI/badge.svg)
+
+A GMail Chrome extension that hides categories without unread messages.
---
+++ src/extension.js
@@ -0,0 +1,56 @@
+"use strict";
+
+import $ from "jquery";
+
+const loaderId = setInterval(() => {
+    if (!window._gmailjs) {
+        return;
+    }
+
+    clearInterval(loaderId);
+    startExtension(window._gmailjs);
+}, 100);
+
+function hideReadCategories() {
+    $.each($(".nU"), (index, element) => {
+        if(! $(element).hasClass('n1')) {
+            const outerNode = $(element.parentNode.parentNode);
+            if (! $(outerNode.children()[0]).hasClass('qj')) {
+                if ($(element).children()[0].getAttribute('tabindex') == '-1') {
+                    outerNode.hide();
+                }
+            }
+        }
+    });
+}
+
+function addButton() {
+    const button = gmail.tools.add_toolbar_button(
+        document.createTextNode('📁'), () => 
+            hideReadCategories(), 
+        'asa'
+    )
+    
+    const observer = new MutationObserver(mutationsList => {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+                    if (mutation.target && mutation.target.style && mutation.target.style.display === 'none') {
+                        mutation.target.style.display = 'block';
+                    }
+            }
+        }
+    });
+
+    observer.observe(button.get(0), {
+        attributes: true,
+        attributeFilter: ['style'],
+    });
+}
+
+function startExtension(gmail) {
+    window.gmail = gmail;    
+
+    gmail.observe.on("load", () => {
+        addButton()
+    });
+}
